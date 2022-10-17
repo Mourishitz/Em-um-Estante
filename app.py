41311306aa25f82533931d191aa59fe8f350763b
Mourishitz <gabrielmorishita@hotmail.com>
2022-10-17 11:56:11 -0300

    🚚 Rotas iniciais de cadastro/listagem de livros

--- app.py
+++ app.py
@@ -1,12 +1,39 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
+
+
+class Book:
+    def __init__(self, name, category, author):
+        self.name = name
+        self.category = category
+        self.author = author
+
+
+bk1 = Book('Clean Code', 'Programacao', 'Robert Cecil')
+bk2 = Book('Arte da Guerra', 'Estrategia', 'Sun Tzu')
+bk3 = Book('Diario de Anne Frank', 'Biografia', 'Anne Frank')
+book_list = [bk1, bk2, bk3]
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():
+    return render_template('list.html', title='Livros', books=book_list)
 
 
-if __name__ == '__main__':
-    app.run()
+@app.route('/new')
+def new():
+    return render_template('new.html', title='Novo Livro')
+
+
+@app.route('/create', methods=['POST', ])
+def create():
+    name = request.form['name']
+    category = request.form['category']
+    author = request.form['author']
+    book = Book(name, category, author)
+    book_list.append(book)
+    return redirect('/')
+
+
+app.run(debug=True)
